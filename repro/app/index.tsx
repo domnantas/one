dc696b903eafb7d0e95083adef064508b2090aa9
domnantas <hello@domnantas.lt>
2025-02-04 23:03:42 +0200

    repro: unable to make local api requests on iOS

--- repro/app/index.tsx
+++ repro/app/index.tsx
@@ -1,6 +1,13 @@
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 
 export default function Index() {
+  const makeRequest = async (url: string) => {
+    const res = await fetch(url)
+    console.log(res)
+    const data = await res.json()
+    console.log(data)
+  }
+
   return (
     <View
       style={{
@@ -9,9 +16,15 @@ export default function Index() {
         justifyContent: 'center',
         alignItems: 'center',
         minHeight: '100%',
+        gap: 12,
       }}
     >
       <Text>Hello world, from One</Text>
+      <Button title="/api/hello" onPress={() => makeRequest('/api/hello')} />
+      <Button
+        title="http://localhost:8081/api/hello"
+        onPress={() => makeRequest('http://localhost:8081/api/hello')}
+      />
     </View>
   )
 }
